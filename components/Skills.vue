a1ad6adda04eaceceedc9088bb5fd0348bcafbe1
Kirill Romanov <58993148+qwerty123t@users.noreply.github.com>
2023-11-11 16:53:29 +0300

    Добавил Навыки

---
+++ components/Skills.vue
@@ -0,0 +1,31 @@
+<template>
+    <section>
+
+        <h2>Навыки</h2>
+        
+        <article>
+            <h4>Adobe Creative Suite</h4>
+            <p>Photoshop, Illustrator, InDesign, XD, After Effects</p>
+        </article>
+        <article>
+            <h4>Prototyping</h4>
+            <p>Sketch, Figma, InVision, Marvel, Principle, Balsamiq</p>
+        </article>
+        <article>
+            <h4>Additional</h4>
+            <p>HTML, CSS, Google Analytics, Keynote, Miro</p>
+        </article>
+        <article>
+            <h4>Languages</h4>
+            <p>Conversational English and Native Russian</p>
+        </article>
+
+    </section>
+</template>
+
+<style scoped lang="scss">
+    section {
+        display: flex;
+    }
+
+</style>
