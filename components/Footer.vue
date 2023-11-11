b28969f059f8d76615623c3b061fd97183e4f88f
Kirill Romanov <58993148+qwerty123t@users.noreply.github.com>
2023-11-11 16:57:17 +0300

    Добавил Футер

---
+++ components/Footer.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <img :src="heart" alt="Rocket preloader" />
+        <button>Top</button>
+    </div>
+</template>
+
+<script setup>
+    import heart from '@/assets/images/heart.png';
+</script>
+
+<style scoped>
+    img {
+        width: 10vw;
+    }
+</style>
