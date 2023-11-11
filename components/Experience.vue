a2b77bb7f33f2f815279f6b69155a602cfb5fe7f
Kirill Romanov <58993148+qwerty123t@users.noreply.github.com>
2023-11-11 14:15:10 +0300

    Добавил опыт работы

---
+++ components/Experience.vue
@@ -0,0 +1,90 @@
+<template>
+    <section>
+        <h2>Опыт работы</h2>
+        <article>
+            <time datetime="2019-07">Июль 2019</time>
+            <div class="position">
+                <h3>Freelance Designer</h3>
+                <div class="first"></div>
+                <div class="second"></div>
+                <div class="third"></div>
+            </div>
+            <p>Netherlands, Worldwide</p>
+            <p>Available for a project or job in UX/UI, Visual and Product design.</p>
+            <a href="#">Commercial tryouts</a>
+            <a href="#">Personal</a>
+        </article>
+
+        <article>
+            <time datetime="2017-01">Январь 2017 - Сентябрь 2018</time>
+            <div class="position">
+                <h3>Digital Designer</h3>
+                <div class="first"></div>
+                <div class="second"></div>
+            </div>
+            <div class="company">
+                <img>
+                <h4>J Creative Solutions</h4>
+                <p>Digital agency in Minsk, Belarus</p>
+            </div>
+            <p>Created visuals for digital marketing channels such as social media, promo web and online ads. Developed visual language from scratch or following the branding guidelines. Designed presentations for clients and indoor purposes.</p>
+            <a href="#">Agency projects</a>
+        </article>
+
+        <article>
+            <time datetime="2014-03">Март 2014 - Февраль 2017</time>
+            <div class="position">
+                <h3>SEO Specialist → Lead SEO Specialist</h3>
+            </div>
+            <div class="company">
+                <img>
+                <h4>OZ.by</h4>
+                <p>Online bookstore in Minsk, Belarus</p>
+            </div>
+            <p>Improved website performance and user experience of the biggest online bookstore OZ in Belarus, being a part of the marketing team, I interpreted Google Analytics data to IT, content and marketing colleagues. Advised on tech and content optimization strategy, automated processes, provided reports, increased organic traffic up to 150%.</p>
+            <a href="#">Annual company report 2016</a>
+        </article>
+
+        <article>
+            <time datetime="2012-08">Август 2012 - Ноябрь 2013</time>
+            <div class="position">
+                <h3>Content Specialist</h3>
+            </div>
+            <div class="company">
+                <img>
+                <h4>21vek.by</h4>
+                <p>E-commerce store in Minsk, Belarus</p>
+            </div>
+            <p>Added and modified the product information, prepared images and text for the website, developed categories filters, ensure all info is correct in the catalogue with the goal of improving online shopper experience.</p>
+        </article>
+
+        
+        <article>
+            <time datetime="2011-08">Август 2011 - Ноябрь 2014</time>
+            <div class="position">
+                <h3>Software Support Engineer</h3>
+                <div class="green"></div>
+            </div>
+            <div class="company">
+                <img>
+                <h4>Integral</h4>
+                <p>Microelectronics factory in Minsk, Belarus</p>
+            </div>
+            <p>Supported retail accounting system LS Trade, troubleshoot technical problems.</p>
+        </article>
+    </section>
+</template>
+
+<style scoped lang="scss">
+    section {
+        display: flex;
+
+        .text {
+        width: 50%;
+        }
+        adress {
+        width: 50%;
+        }
+    }
+
+</style>
